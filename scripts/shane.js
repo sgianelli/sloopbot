3fdbc874c82bcd0ca1ffafbc8cbff0ee3c1f5b8d
sjdev <shanejgianelli@gmail.com>
2016-08-01 10:08:15 -0700

    redditbot

--- scripts/shane.js
+++ scripts/shane.js
@@ -1,4 +1,8 @@
 module.exports = function (robot) {
+    robot.hear(/r\/(\w+)/i, function (response) {
+        response.send("http://reddit.com/" + response.match[0]);
+    });
+
     robot.listen(function (message) {
         return message.user.name == "shane" && message.text.toLowerCase() == "rumba?";
     }, function (response) {
