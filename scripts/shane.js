11d668076ca74c776c243118a160bcf1e0ee1f79
sjdev <shanejgianelli@gmail.com>
2016-08-01 16:26:04 -0700

    Cleanup

--- scripts/shane.js
+++ scripts/shane.js
@@ -1,15 +1,4 @@
 module.exports = function (robot) {
-    robot.listen(function (message) {
-        for (key in message) {
-            console.log(" - " + key + " : " + message[key]);
-        }
-
-        robot.emit('slack.reaction', { message: message, name: "+1" });
-
-        return message.text == "slutty";
-    }, function (response) {
-    });
-
     robot.hear(/^r\/(\w+)$/i, function (response) {
         response.send("http://reddit.com/" + response.match[0]);
     });
